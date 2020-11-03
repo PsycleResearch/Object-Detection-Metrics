d13a361d94d064fb5dc74ee2fa0503c27b8c6d63
Arunavameister <arunavameister@yahoo.com>
2020-11-03 16:05:11 +0100

    Transforming into Library.

---
+++ Object-Detection-Metrics/BoundingBoxes.py
@@ -0,0 +1,90 @@
+import os,sys
+currentPath = os.path.dirname(os.path.abspath(__file__))
+
+#################
+# def add_path(path):
+#     if path not in sys.path:
+#         sys.path.insert(0, path)
+#
+# # Add lib to PYTHONPATH
+# libPath = os.path.join(currentPath, 'lib')
+# add_path(libPath)
+#################
+
+from BoundingBox import *
+from utils import *
+
+
+class BoundingBoxes:
+    def __init__(self):
+        self._boundingBoxes = []
+
+    def addBoundingBox(self, bb):
+        self._boundingBoxes.append(bb)
+
+    def removeBoundingBox(self, _boundingBox):
+        for d in self._boundingBoxes:
+            if BoundingBox.compare(d, _boundingBox):
+                del self._boundingBoxes[d]
+                return
+
+    def removeAllBoundingBoxes(self):
+        self._boundingBoxes = []
+
+    def getBoundingBoxes(self):
+        return self._boundingBoxes
+
+    def getBoundingBoxByClass(self, classId):
+        boundingBoxes = []
+        for d in self._boundingBoxes:
+            if d.getClassId() == classId:  # get only specified bounding box type
+                boundingBoxes.append(d)
+        return boundingBoxes
+
+    def getClasses(self):
+        classes = []
+        for d in self._boundingBoxes:
+            c = d.getClassId()
+            if c not in classes:
+                classes.append(c)
+        return classes
+
+    def getBoundingBoxesByType(self, bbType):
+        # get only specified bb type
+        return [d for d in self._boundingBoxes if d.getBBType() == bbType]
+
+    def getBoundingBoxesByImageName(self, imageName):
+        # get only specified bb type
+        return [d for d in self._boundingBoxes if d.getImageName() == imageName]
+
+    def count(self, bbType=None):
+        if bbType is None:  # Return all bounding boxes
+            return len(self._boundingBoxes)
+        count = 0
+        for d in self._boundingBoxes:
+            if d.getBBType() == bbType:  # get only specified bb type
+                count += 1
+        return count
+
+    def clone(self):
+        newBoundingBoxes = BoundingBoxes()
+        for d in self._boundingBoxes:
+            det = BoundingBox.clone(d)
+            newBoundingBoxes.addBoundingBox(det)
+        return newBoundingBoxes
+
+    def drawAllBoundingBoxes(self, image, imageName):
+        bbxes = self.getBoundingBoxesByImageName(imageName)
+        for bb in bbxes:
+            if bb.getBBType() == BBType.GroundTruth:  # if ground truth
+                image = add_bb_into_image(image, bb, color=(0, 255, 0))  # green
+            else:  # if detection
+                image = add_bb_into_image(image, bb, color=(255, 0, 0))  # red
+        return image
+
+    # def drawAllBoundingBoxes(self, image):
+    #     for gt in self.getBoundingBoxesByType(BBType.GroundTruth):
+    #         image = add_bb_into_image(image, gt ,color=(0,255,0))
+    #     for det in self.getBoundingBoxesByType(BBType.Detected):
+    #         image = add_bb_into_image(image, det ,color=(255,0,0))
+    #     return image
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Object-Detection-Metrics',
+      version='1.0',
+      author="Psycle Research",
+      description="Object Detection statistics library forked from rafaelpadilla's repo",
+      url="https://github.com/PsycleResearch/Object-Detection-Metrics",
+      packages=['Object-Detection-Metrics'],
+      python_requires='>=3.6'
+      )
