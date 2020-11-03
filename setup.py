f92995d5afb734c8cd72d72d497c2267609bcf71
Arunavameister <arunavameister@yahoo.com>
2020-11-03 16:16:22 +0100

    Library creation.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='Object-Detection-Metrics',
+setup(name='Object_Detection_Metrics',
       version='1.0',
       author="Psycle Research",
       description="Object Detection statistics library forked from rafaelpadilla's repo",
       url="https://github.com/PsycleResearch/Object-Detection-Metrics",
-      packages=['Object-Detection-Metrics'],
+      packages=['Object_Detection_Metrics'],
       python_requires='>=3.6'
       )
